869a50006da722c9d7ee825454d19834aaadda04
Mark Pude <markpude@gmail.com>
2018-03-23 16:02:33 -0400

    added -y's

--- docker.sh
+++ docker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
  sudo apt update
- sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+ sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
  curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable"
  sudo apt update
- sudo apt install docker-ce
+ sudo apt install docker-ce -y
 
  # Test it
  sudo docker run hello-world
