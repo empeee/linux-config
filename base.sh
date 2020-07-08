09a61da1532ab41e53c4402012b0a2cc0c9cae42
Mark Pude <markpude@gmail.com>
2020-07-07 21:46:03 -0400

    cleaned up, call the vim_setup.sh script in dotfiles instead of repeating stuff

--- base.sh
+++ base.sh
@@ -7,16 +7,8 @@ sudo apt upgrade -y
 sudo apt install vim tmux htop iftop mc -y
 
 # Git clones
-mkdir -p ~/.vim/tmp
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/empeee/dotfiles ~/dotfiles
-git clone https://github.com/powerline/fonts.git ~/fonts --depth=1
 
 # Run setups
 ~/dotfiles/makesymlinks.sh
-cd ~/fonts
-./install.sh
-
-# Cleanup
-cd
-rm -rf fonts
+~/dotfiles/vim_setup.sh
