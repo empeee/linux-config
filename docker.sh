54f0b6c2be3305af55c265ccbc984fa68f97d42d
Mark Pude <markpude@gmail.com>
2020-07-07 21:46:35 -0400

    updated instructions as of 7/7/20

--- docker.sh
+++ docker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
  sudo apt update
- sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
+ sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
  curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable"
  sudo apt update
- sudo apt install docker-ce -y
+ sudo apt install docker-ce docker-ce-cli containerd.io -y
 
  # Test it
  sudo docker run hello-world
