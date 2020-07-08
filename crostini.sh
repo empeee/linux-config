b311c82594728a1b61d826533232aa6168a9d829
Mark Pude <markpude@gmail.com>
2020-07-08 09:53:35 -0400

    cleaned up and tested on a fresh crostini install

--- crostini.sh
+++ crostini.sh
@@ -4,7 +4,8 @@ sudo apt update
 sudo apt upgrade -y
 
 # Most things are simple apt installs
-sudo apt install terminator python3 python3-pip idle3 python-numpy python-scipy python-matplotlib ipython jupyter python-pandas python-sympy python-nose gimp -y
+sudo apt install terminator gimp python3 python3-pip idle3 -y
+python3 -m pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose
 
 # VS Code needs a little work
 curl -L "https://go.microsoft.com/fwlink/?LinkID=760868" > ~/vscode.deb
