62cc819a9b1cf4ca64b98fdfa27a92d3587ed925
Mark Pude <markpude@gmail.com>
2020-07-08 20:07:19 -0400

    changed to venv for python stuff

--- crostini.sh
+++ crostini.sh
@@ -4,8 +4,19 @@ sudo apt update
 sudo apt upgrade -y
 
 # Most things are simple apt installs
-sudo apt install terminator gimp python3 python3-pip idle3 -y
-python3 -m pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose
+sudo apt install terminator gimp -y
+
+# Python clean install
+sudo apt install python3 python3-pip python3-venv idle3 -y
+
+# Make python venvs
+mkdir -p ~/python-venv && cd ~/python-venv
+
+# Make default scipy venv
+python3 -m venv scipy
+source scipy/bin/activate
+pip install numpy scipy matplotlib ipython jupyter pandas sympy nose
+deactivate
 
 # VS Code needs a little work
 curl -L "https://go.microsoft.com/fwlink/?LinkID=760868" > ~/vscode.deb
